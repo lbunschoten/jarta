5bc7ffeb0946116f3d43ae86c755aea3f0aa58f1
Leon Bunschoten <lbunschoten@users.noreply.github.com>
2016-12-11 13:34:44 +0100

    Setting up for running the first service

---
+++ thought-service/server/src/main/scala/jarta/thoughtservice/ThoughtServiceServer.scala
@@ -0,0 +1,22 @@
+package jarta.thoughtservice
+
+import com.twitter.finatra.thrift.ThriftServer
+import com.twitter.finatra.thrift.filters._
+import com.twitter.finatra.thrift.routing.ThriftRouter
+
+object ThoughtServiceServer extends FinatraThoughtServiceServer
+
+trait FinatraThoughtServiceServer extends ThriftServer {
+
+  override val name = "thought-service-server"
+  override val disableAdminHttpServer = true
+
+  override def configureThrift(router: ThriftRouter) {
+    router
+      .filter[LoggingMDCFilter]
+      .filter[TraceIdMDCFilter]
+      .filter[ThriftMDCFilter]
+      .filter[AccessLoggingFilter]
+      .filter[StatsFilter]
+  }
+}
