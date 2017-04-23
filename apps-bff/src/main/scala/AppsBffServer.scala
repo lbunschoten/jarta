9ea522c5a20e4e322c66ca4bea44ad52c711823c
Leon Bunschoten <lbunschoten@users.noreply.github.com>
2017-04-23 17:40:51 +0200

    Made it slightly easier to run the service locally, as well as from kubernetes

--- apps-bff/src/main/scala/AppsBffServer.scala
+++ apps-bff/src/main/scala/AppsBffServer.scala
@@ -14,14 +14,17 @@ object AppsBffServer extends HttpServer {
   override protected def disableAdminHttpServer = true
   override val defaultFinatraHttpPort = ":8891"
 
-  private def thriftClientBuilder(port: Int) = ClientBuilder()
-    .hosts(Seq(new InetSocketAddress("jarta", port)))
+  private val thoughtServiceHost = flag(name = "thought.service.host", default = "localhost", help = "Host for thought-service")
+  private val thoughtServicePort = flag(name = "thought.service.port", default = 9997, help = "Host for thought-service")
+
+  private def thriftClientBuilder(host: String, port: Int) = ClientBuilder()
+    .hosts(Seq(new InetSocketAddress(host, port)))
     .codec(ThriftClientFramedCodec())
     .hostConnectionLimit(1)
     .failFast(false)
     .build()
 
-  def thoughtService: ThoughtService.FutureIface = new ThoughtService.FinagledClient(thriftClientBuilder(port = 9997))
+  def thoughtService: ThoughtService.FutureIface = new ThoughtService.FinagledClient(thriftClientBuilder(host = thoughtServiceHost(), port = thoughtServicePort()))
 
   override protected def configureHttp(router: HttpRouter): Unit = {
     router
